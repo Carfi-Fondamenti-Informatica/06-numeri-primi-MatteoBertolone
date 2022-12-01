ccaf0c52176f44f6e85ba36c8a7ed0491425eed4
MatteoBertolone <114439478+MatteoBertolone@users.noreply.github.com>
2022-12-01 12:40:41 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
 
+bool numprimo(int i, int a){
+    if((a%i)==0){
+        if(a==i) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+    numprimo(i+1,a);
+}
