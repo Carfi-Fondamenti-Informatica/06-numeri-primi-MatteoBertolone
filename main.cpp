f6c29a09925636097e20cbc5d2a92021d8304160
MatteoBertolone <114439478+MatteoBertolone@users.noreply.github.com>
2022-12-01 12:38:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
 int main(){
+    int n=0,i=2;
+    cin>>n;
+    if (numprimo(i, n)) {
+        cout << "numero primo" << endl;
+    }else{
+        cout <<"numero non primo"<< endl;}
   return 0;
 }
